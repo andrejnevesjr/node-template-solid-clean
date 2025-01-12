d98bfb6e2b15cf20f7ed6ec5bfed21d34d6fb428
André Junior <andre.nevesjunior@scopely.com>
2025-01-12 12:36:27 +0000

    feat: adding JEST test for logout, fail without Cookie and Listing Products with a Cookie

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -8,6 +8,12 @@ import { ProductRepositoryPrisma } from '../src/infra/repositories/product/produ
 import { UserRepositoryPrisma } from '../src/infra/repositories/user/user.repository.prisma';
 
 import { prismaMock } from './singleton';
+import { CreateUserRoute } from '../src/infra/api/express/routes/users/create-user.express.route';
+import { LoginUserRoute } from '../src/infra/api/express/routes/users/login-user.express.route';
+import { LogoutUserRoute } from '../src/infra/api/express/routes/users/logout-user.express.route';
+import { CreateUserUseCase } from '../src/usecases/user/create-user.usecase';
+import { LoginUserUseCase } from '../src/usecases/user/login-user.usecase';
+import { LogoutUserUseCase } from '../src/usecases/user/logout-user.usecase';
 
 describe('API Tests', () => {
   let api: ApiExpress;
@@ -36,23 +42,48 @@ describe('API Tests', () => {
       },
     ],
   };
+
   beforeAll(() => {
-    const aRepository = ProductRepositoryPrisma.create(prismaMock);
+    const aProductRepository = ProductRepositoryPrisma.create(prismaMock);
     const aUserRepository = UserRepositoryPrisma.create(prismaMock);
 
     const createProductUseCase = CreateProductUseCase.create(
-      aRepository,
+      aProductRepository,
       aUserRepository,
     );
-    const listProductUseCase = ListProductUseCase.create(aRepository);
+    const listProductUseCase = ListProductUseCase.create(aProductRepository);
+    const createUserUseCase = CreateUserUseCase.create(aUserRepository);
+    const loginUserUseCase = LoginUserUseCase.create(aUserRepository);
+    const logoutUserUseCase = LogoutUserUseCase.create(aUserRepository);
 
-    const createRoute = CreateProductRoute.create(createProductUseCase);
-    const listRoute = ListProductRoute.create(listProductUseCase);
+    const createProductRoute = CreateProductRoute.create(createProductUseCase);
+    const listProductRoute = ListProductRoute.create(listProductUseCase);
+    const createUserRoute = CreateUserRoute.create(createUserUseCase);
+    const loginUserRoute = LoginUserRoute.create(loginUserUseCase);
+    const logoutUserRoute = LogoutUserRoute.create(logoutUserUseCase);
 
-    api = ApiExpress.create([createRoute, listRoute]);
+    api = ApiExpress.create([
+      createProductRoute,
+      listProductRoute,
+      createUserRoute,
+      loginUserRoute,
+      logoutUserRoute,
+    ]);
   });
 
-  it('should create a product', async () => {
+  it('should logout a user', async () => {
+    const response = await request(api.getApp())
+      .post('/users/logout')
+      .set('Cookie', 'token=123456');
+
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual({
+      success: true,
+      message: 'logged out',
+    });
+  });
+
+  it('should fail when attempt to create a product', async () => {
     const response = await request(api.getApp())
       .post('/products')
       .send({ name: 'Test Product', price: 100 });
@@ -60,29 +91,23 @@ describe('API Tests', () => {
     expect(response.status).toBe(401);
   });
 
-  // it('should create a product', async () => {
-  //   const response = await request(api.getApp())
-  //     .post('/products')
-  //     .send({ name: 'Test Product', price: 100 });
+  it('should list products', async () => {
+    prismaMock.product.findMany.mockResolvedValue(products.products);
+    const response = await request(api.getApp())
+      .get('/products')
+      .set('Cookie', 'token=123456');
 
-  //   expect(response.status).toBe(201);
-  //   expect(response.body).toHaveProperty('id');
-  // });
-
-  // it('should list products', async () => {
-  //   prismaMock.product.findMany.mockResolvedValue(products.products);
-  //   const response = await request(api.getApp()).get('/products');
-  //   expect(response.status).toBe(200);
-  //   expect(response.body).toEqual(
-  //     expect.objectContaining({
-  //       products: expect.arrayContaining([
-  //         expect.objectContaining({
-  //           id: expect.any(String),
-  //           name: expect.any(String),
-  //           price: expect.any(Number),
-  //         }),
-  //       ]),
-  //     }),
-  //   );
-  // });
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual(
+      expect.objectContaining({
+        products: expect.arrayContaining([
+          expect.objectContaining({
+            name: expect.any(String),
+            price: expect.any(Number),
+            user_id: expect.any(String),
+          }),
+        ]),
+      }),
+    );
+  });
 });
